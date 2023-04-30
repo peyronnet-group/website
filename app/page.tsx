83daf37c5adeb1d923d71ce0ae7fb87d05d356b1
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-04-30 11:25:58 +0200

    Added privacy page

--- app/page.tsx
+++ app/page.tsx
@@ -7,62 +7,9 @@ import { motion, useInView } from "framer-motion"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import SiteFooter from "@/components/footer"
+import { AnimatedLink, Paragraph, Title } from "@/components/text"
 
 export default function IndexPage() {
-  function Title({ children }) {
-    const ref = useRef(null)
-    const isInView = useInView(ref, { once: true })
-
-    return (
-      <h1
-        ref={ref}
-        className="font-wide text-3xl uppercase leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl"
-        style={{
-          transform: isInView ? "none" : "translateX(-200px)",
-          opacity: isInView ? 1 : 0,
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-        }}
-      >
-        {children}
-      </h1>
-    )
-  }
-
-  function Paragraph({ children }) {
-    const ref = useRef(null)
-    const isInView = useInView(ref, { once: true })
-    return (
-      <p
-        ref={ref}
-        className="font-serif text-lg text-muted-foreground sm:text-xl"
-        style={{
-          opacity: isInView ? 1 : 0,
-          transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
-        }}
-      >
-        {children}
-      </p>
-    )
-  }
-
-  function AnimatedLink({ children, href }) {
-    const ref = useRef(null)
-    const isInView = useInView(ref, { once: true })
-    return (
-      <Link
-        ref={ref}
-        className="hover:underline"
-        href={href}
-        style={{
-          opacity: isInView ? 1 : 0,
-          transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1) 2s",
-        }}
-      >
-        {children}
-      </Link>
-    )
-  }
-
   function CompanyCard(props) {
     const ref = useRef(null)
     const isInView = useInView(ref, { once: true })
