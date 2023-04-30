6befe079e502ffba1d517334e46c4ac85b327ced
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-04-30 10:35:06 +0200

    Improved footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,12 +5,12 @@ import Logo from "./logo"
 export default function SiteFooter() {
   return (
     <footer className="flex flex-col justify-center space-y-2 px-5 py-10 sm:grid sm:grid-cols-3">
-      <div className="flex items-center">
+      <div className="flex items-center justify-center sm:justify-normal">
         <Link href="/">
           <Logo width={256} height={64} />
         </Link>
       </div>
-      <div>
+      <div className="m-4 sm:m-0">
         <h3 className="font-wide text-lg uppercase leading-tight tracking-tighter">
           Links
         </h3>
@@ -26,15 +26,21 @@ export default function SiteFooter() {
           </Link>
         </div>
       </div>
-      <div>
+      <div className="m-4 sm:m-0">
         <h3 className="font-wide text-lg uppercase leading-tight tracking-tighter">
           Socials
         </h3>
         <div className="flex flex-col">
-          <Link className="hover:underline" href={""}>
+          <Link
+            className="hover:underline"
+            href={"https://twitter.com/PeyronnetGroup"}
+          >
             Twitter
           </Link>
-          <Link className="hover:underline" href={""}>
+          <Link
+            className="hover:underline"
+            href={"https://www.youtube.com/@PeyronnetGroup"}
+          >
             YouTube
           </Link>
           <Link className="hover:underline" href={""}>
